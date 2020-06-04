81e96b4aad4f96cc60d18596e8013a974fb2ba49
Brandon Delaportas <b.delaportas@gmail.com>
2020-06-04 18:25:11 -0400

    Basic front-end layout.  subject to change.

---
+++ src/data.js
@@ -0,0 +1,16 @@
+/*Cactus Icon: https://www.iconfinder.com/haneka */
+
+export const StoreProducts = [
+{"id":1,"first_name":"Adena","pic":"https://robohash.org/eaquelaboriosamdolores.png?size=50x50&set=set1", 'inCart': false},
+{"id":2,"first_name":"Ervin","pic":"https://robohash.org/deseruntenimqui.png?size=50x50&set=set1", 'inCart': false},
+{"id":3,"first_name":"Ermina","pic":"https://robohash.org/sinteumamet.png?size=50x50&set=set1", 'inCart': false},
+{"id":4,"first_name":"Merv","pic":"https://robohash.org/hicexercitationemblanditiis.bmp?size=50x50&set=set1", 'inCart': false},
+{"id":5,"first_name":"Julina","pic":"https://robohash.org/voluptatesautminima.png?size=50x50&set=set1", 'inCart': false},
+{"id":6,"first_name":"Meir","pic":"https://robohash.org/voluptasadquia.jpg?size=50x50&set=set1", 'inCart': false},
+{"id":7,"first_name":"Rolph","pic":"https://robohash.org/quinullamolestiae.jpg?size=50x50&set=set1", 'inCart': false},
+{"id":8,"first_name":"Der","pic":"https://robohash.org/voluptatibusquiaconsequatur.bmp?size=50x50&set=set1", 'inCart': false},
+{"id":9,"first_name":"Aubrey","pic":"https://robohash.org/excepturivoluptatemporro.png?size=50x50&set=set1", 'inCart': false},
+{"id":10,"first_name":"Dido","pic":"https://robohash.org/enimutsaepe.bmp?size=50x50&set=set1", 'inCart': false
+}]
+
+export const DetailProducts =[{id :6,"first_name":"Meir","pic":"https://robohash.org/voluptasadquia.jpg?size=50x50&set=set1", 'inCart': false}]
