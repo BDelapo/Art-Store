9602a799a80a638984a1ca545a0f3aebd11b972c
unknown <tdelaportas@hotmail.com>
2020-07-31 17:12:38 -0400

    Added nodemon as well as a nodemon startup script

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const { response } = require('express')
 const app = express()
 
+const port = 5000
+
 app.get('/', (req, res) =>{
 
     return res.send("This is a test")
 })
 
-app.listen(3001, ()=>{
-    console.log('server is up and listening on port 3001')
+app.listen(port, ()=>{
+    console.log('Server is up and listening on port '+ port)
 })
