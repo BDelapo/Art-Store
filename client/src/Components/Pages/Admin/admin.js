66ae527ca62ad945c68267be24f7e591058a133e
Brandon Delaportas <b.delaportas@gmail.com>
2020-08-30 15:27:51 -0400

    Updated admin page to make more accessible

--- client/src/Components/Pages/Admin/admin.js
+++ client/src/Components/Pages/Admin/admin.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import PopUp from './testImage'
+import RemoveFromCart from '../../Buttons/RemoveFromCart';
 
 
-const  Admin = () => {
+
+const Admin = () => {
 
     const [file, setFile] = useState(null)
     const [fileName, setFileName] = useState(null)
@@ -12,13 +14,53 @@ const  Admin = () => {
     const [img, setImg] = useState(null)
     const [name, setName] = useState(null)
 
+    const [products, setProducts] = useState([])
 
+    useEffect(()=>{
+        async function fetchData(){
+             await fetch(`http://localhost:5000/graphql`, {
+                method: 'POST',
+                headers: {'Content-Type' :'application/json'},
+                body: JSON.stringify({
+                    query: `{getAllProducts{id, productName, price, description, image}}`
+                })
+            }).then((response) => response.json().then((data) => setProducts(data.data.getAllProducts)))
+        }
+        fetchData()
+    },[])
+
+    console.log(products)
+
+    const list = () => {
+        if (!products) {
+            return (
+                <div className="ui segment">
+                    <div className="ui active dimmer">
+                        <div className="ui loader"></div>
+                    </div>
+                </div>
+            )
+        }
+        else {
+            return products.map(item => {
+                return (
+                    <tr>
+                        <td>{item.id}</td>
+                        <td><img src={item.image} alt='unavailable' /></td>
+                        <td>{item.productName}</td>
+                        <td>{item.price}</td>
+                        <td><button onClick={removeProductHandler}>X</button></td>
+                    </tr>
+                )
+            })
+        }
+    }
 
     const fileSelectHandler = event => {
         setFileName(event.target.files[0].name)
         const reader = new FileReader()
         reader.readAsDataURL(event.target.files[0])
-        reader.onload=(e)=>{
+        reader.onload = (e) => {
             setFile(e.target.result)
         }
     }
@@ -37,31 +79,30 @@ const  Admin = () => {
     }
 
 
-
     async function fileUploadHandler() {
-			await fetch(`http://localhost:5000/graphql`, {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify({
-					query: `mutation {addProduct(productName:"${fileName}", price:"${price}", description:"${description}", image:"${file}"){productName}}`
-                        })
-                    })
-                .then((response) => response.json().then((data)=>{console.log(data.data.getProduct)}));
-                console.log("bublewrap")
+        await fetch(`http://localhost:5000/graphql`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                query: `mutation {addProduct(productName:"${fileName}", price:"${price}", description:"${description}", image:"${file}"){productName}}`
+            })
+        })
+            .then((response) => response.json().then((data) => { console.log(data.data.getProduct) }));
+        console.log("bublewrap")
     }
 
     async function fileDownloadHandler() {
-			await fetch(`http://localhost:5000/graphql`, {
-				method: 'POST',
-				headers: { 'Content-Type'  :'application/json' },
-				body: JSON.stringify({
-					query: `{getProduct(productName:"${name}") {image}}`
-                        })
-                    })
-                .then((response) => response.json().then((data => setImg(data.data.getProduct[0].image))));
+        await fetch(`http://localhost:5000/graphql`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                query: `{getProduct(productName:"${name}") {image}}`
+            })
+        })
+            .then((response) => response.json().then((data => setImg(data.data.getProduct[0].image))));
     }
 
-    async function removeProductHandler(){
+    async function removeProductHandler() {
         await fetch(`http://localhost:5000/graphql`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -69,7 +110,7 @@ const  Admin = () => {
                 query: `mutation {removeProduct(productName:"${name}") {productName}}`
             })
         })
-        .then((response) => response.json().then((data => console.log(data))))
+            .then((response) => response.json().then((data => console.log(data))))
     }
 
 
@@ -78,35 +119,48 @@ const  Admin = () => {
         <div>
             <div className='ui container cart-container'>
                 <div className='ui centered grid'>
-                    <div className='twelve wide column'>
-                        <div className='ui segment'>
-                            <table className="ui selectable celled table">
-                                <thead>
-                                    <tr>
-                                     <th><input type ="file" onChange={fileSelectHandler}/></th>
-                                     <th><input type="text" onChange={priceHandler}/>Price</th>
-                                     <th><input type="text" onChange={descriptionHandler}/>Description</th>
-                                     <th><button onClick={fileUploadHandler}>UPLOAD Product to database</button></th>
-
-                                    </tr>
-                                </thead>
-                            </table>
-                            <table className="ui selectable celled table">
-                                <thead>
-                                    <tr>
-                                     <th> <input type='text' onChange={textHandler}/>Product name</th>
-                                     <th> <button onClick={fileDownloadHandler}>DOWNLOAD Product from database</button></th>
-                                     <th><button onClick ={removeProductHandler}>DELETE Product from Database</button></th>
-                                    </tr>
-                                </thead>
-                            </table>
-                            <PopUp imgsrc ={img}/>
+                    <div className='fourteen wide column'>
+                        <div class="ui form">
+                            <h4 class="ui dividing header">Upload Product</h4>
+                            <div class="four fields">
+                                <div class="field">
+                                    <label>Image</label>
+                                    <input type="file" onChange={fileSelectHandler} />
+                                </div>
+                                <div class="field">
+                                    <label>Price</label>
+                                    <input type="text" onChange={priceHandler} />
+                                </div>
+                                <div class="field">
+                                    <label>Descrription</label>
+                                    <input type="text" onChange={descriptionHandler} />
+                                </div>
+                                <div class="field">
+                                    <label>Complete</label>
+                                    <button onClick={fileUploadHandler}>UPLOAD PRODUCT</button>
+                                </div>
+                            </div>
                         </div>
+                        <h4 class="ui dividing header">Current Products</h4>
+                        <table class="ui celled table">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Image</th>
+                                    <th>Name</th>
+                                    <th>Price</th>
+                                    <th>Remove</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {list()}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-     )
+    )
 }
 
 export default Admin
